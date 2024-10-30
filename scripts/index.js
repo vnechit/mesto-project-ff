878a59d396b3d4fff21926324959e4176222f4d0
vnechit <vnechit310301@gmail.com>
2024-10-31 02:35:36 +0300

    done

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+// DOM узлы
+const placesList = document.querySelector('.places__list');
+//  Функция создания карточки
+function createCard (card) {
+    const clonedCard = cardTemplate.querySelector('.places__item').cloneNode(true);
+    clonedCard.querySelector('.card__title').textContent = card.name;
+    clonedCard.querySelector('.card__image').src = card.link;
+    clonedCard.querySelector('.card__delete-button').addEventListener('click', (event) => {
+        deleteCard(event)
+    });
+    return clonedCard;
+}
+// Функция удаления карточки
+function deleteCard (event) {
+    const itemToDelete = event.target.parentElement;
+    itemToDelete.remove();
+}
+// Вывести карточки на страницу
+initialCards.forEach((item) => {
+    const createdCard = createCard(item);
+    placesList.append(createdCard);
+});
