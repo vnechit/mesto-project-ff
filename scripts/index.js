aabf01ac28d33725ab04f22deb797416047e76ea
vnechit <vnechit310301@gmail.com>
2024-10-31 13:56:10 +0300

    fixes

--- scripts/index.js
+++ scripts/index.js
@@ -3,22 +3,24 @@ const cardTemplate = document.querySelector('#card-template').content;
 // DOM узлы
 const placesList = document.querySelector('.places__list');
 //  Функция создания карточки
-function createCard (card) {
+function createCard (card, deleteCardFunc) {
     const clonedCard = cardTemplate.querySelector('.places__item').cloneNode(true);
     clonedCard.querySelector('.card__title').textContent = card.name;
-    clonedCard.querySelector('.card__image').src = card.link;
+    const image = clonedCard.querySelector('.card__image');
+    image.src = card.link;
+    image.alt = card.name;
     clonedCard.querySelector('.card__delete-button').addEventListener('click', (event) => {
-        deleteCard(event)
+        deleteCardFunc(event)
     });
     return clonedCard;
 }
 // Функция удаления карточки
 function deleteCard (event) {
-    const itemToDelete = event.target.parentElement;
+    const itemToDelete = event.target.closest('.places__item');
     itemToDelete.remove();
 }
 // Вывести карточки на страницу
 initialCards.forEach((item) => {
-    const createdCard = createCard(item);
+    const createdCard = createCard(item, deleteCard);
     placesList.append(createdCard);
 });
